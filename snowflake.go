683edec3e7aa7c344ec8773eec3838b2f684517a
Anders Øen Fylling <fyllingz@gmail.com>
2018-01-19 03:14:38 +0100

    Added Un/MarshalText and Un/MarshalBinary

--- snowflake.go
+++ snowflake.go
@@ -1,6 +1,8 @@
 package snowflake
 
-import "strconv"
+import (
+	"strconv"
+)
 
 // NewID creates a new Snowflake ID from a uint64.
 func NewID(id uint64) ID {
@@ -52,7 +54,33 @@ func (s ID) HexPrettyString() string {
 	return "0x" + strconv.FormatUint(uint64(s), 16)
 }
 
-// BinaryString create a binary literal representation as a string
-func (s ID) BinaryString() string {
-	return strconv.FormatUint(uint64(s), 2)
+// MarshalBinary create a binary literal representation as a string
+func (s ID) MarshalBinary() (data []byte, err error) {
+	return []byte(strconv.FormatUint(uint64(s), 2)), nil
+}
+func (s *ID) UnmarshalBinary(text []byte) (err error) {
+	id, err := strconv.ParseUint(string(text), 2, 64)
+	if err != nil {
+		return
+	}
+	*s = ID(id)
+
+	return
+}
+
+func (s ID) MarshalText() (text []byte, err error) {
+	text = []byte(s.String())
+	err = nil
+
+	return
+}
+
+func (s *ID) UnmarshalText(text []byte) (err error) {
+	id, err := strconv.ParseUint(string(text), 10, 64)
+	if err != nil {
+		return
+	}
+	*s = ID(id)
+
+	return
 }
--- snowflake_test.go
+++ snowflake_test.go
@@ -1,8 +1,12 @@
 package snowflake
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestString(t *testing.T) {
+	var b []byte
+	var err error
 	id := NewID(uint64(435834986943))
 	if id.String() != "435834986943" {
 		t.Errorf("String conversion failed. Got %s, wants %s", id.String(), "435834986943")
@@ -20,8 +24,12 @@ func TestString(t *testing.T) {
 		t.Errorf("String conversion for Pretty Hex failed. Got %s, wants %s", id.HexPrettyString(), "0x6579ca21bf")
 	}
 
-	if id.BinaryString() != "110010101111001110010100010000110111111" {
-		t.Errorf("String conversion for binary failed. Got %s, wants %s", id.BinaryString(), "110010101111001110010100010000110111111")
+	b, err = id.MarshalBinary()
+	if err != nil {
+		t.Error(err)
+	}
+	if string(b) != "110010101111001110010100010000110111111" {
+		t.Errorf("String conversion for binary failed. Got %s, wants %s", string(b), "110010101111001110010100010000110111111")
 	}
 }
 
@@ -29,5 +37,41 @@ func TestEmpty(t *testing.T) {
 	id := NewID(0)
 	if !id.Empty() {
 		t.Errorf("Expects ID to be viewed as empty when value is 0")
+	}
+}
+
+func TestBinaryMarshalling(t *testing.T) {
+	id := NewID(4598345)
+	b, err := id.MarshalBinary()
+	if err != nil {
+		t.Error(err)
+	}
+
+	id2 := NewID(4534)
+	err = id2.UnmarshalBinary(b)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if id2 != id {
+		t.Errorf("Value differs. Got %d, wants %d", id2, id)
+	}
+}
+
+func TestTextMarshalling(t *testing.T) {
+	id := NewID(4598345)
+	b, err := id.MarshalText()
+	if err != nil {
+		t.Error(err)
+	}
+
+	id2 := NewID(4534)
+	err = id2.UnmarshalText(b)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if id2 != id {
+		t.Errorf("Value differs. Got %d, wants %d", id2, id)
 	}
 }
